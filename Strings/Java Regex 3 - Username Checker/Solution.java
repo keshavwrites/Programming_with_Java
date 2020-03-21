f9ee5f3ee740cb024d501c19a8c8746eae7edc47
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-03-21 18:11:04 +0200

    Update Solution.java

--- Strings/Java Regex 3 - Username Checker/Solution.java
+++ Strings/Java Regex 3 - Username Checker/Solution.java
@@ -13,7 +13,7 @@ public class Solution {
       while (testCases > 0) {
          String username = in.nextLine();
          String pattern = "^[a-zA-Z]\\w{7,29}$";
-
+         
          Pattern r = Pattern.compile(pattern);
          Matcher m = r.matcher(username);
           
@@ -25,4 +25,29 @@ public class Solution {
          testCases--;
       }
    }
+}
+
+//Second Solution
+
+import java.util.Scanner;
+class UsernameValidator {
+    public static String regularExpression = 
+    "^[a-zA-Z][a-zA-Z0-9_]{7,29}$";
+}
+
+public class Solution {
+    private static final Scanner scan = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        int n = Integer.parseInt(scan.nextLine());
+        while (n-- != 0) {
+            String userName = scan.nextLine();
+
+            if (userName.matches(UsernameValidator.regularExpression)) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }           
+        }
+    }
 }
