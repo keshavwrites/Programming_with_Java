cdbe223e870199c06e6f749c62223a3e987d30c8
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-03-21 12:57:02 +0200

    Update String Split Tokens.java

--- Practice/String Split Tokens.java
+++ Practice/String Split Tokens.java
@@ -8,16 +8,16 @@ public class Solution {
         Scanner scan = new Scanner(System.in);
         String s = scan.nextLine();
 
-        String[] items = s.trim().split("[ !,?._'@]+");
+        String[] tokens = s.trim().split("[ !,?._'@]+");
 
         if (s == null || s.trim().equals("")) {
             System.out.println("0");
         } else if (s.length() > 400000) {
             return;
         } else {
-            System.out.println(items.length);
+            System.out.println(tokens.length);
         }
-        for (String item : items) {
+        for (String item : tokens) {
             System.out.println(item);
         }
         scan.close();
