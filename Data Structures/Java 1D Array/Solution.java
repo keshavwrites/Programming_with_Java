0b32a74b3be782bf17a632646a6c520c32c1d3f1
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-03-21 19:03:34 +0200

    Update Solution.java

--- Data Structures/Java 1D Array/Solution.java
+++ Data Structures/Java 1D Array/Solution.java
@@ -18,3 +18,27 @@ public class Solution {
         }
     }
 }
+
+//Second Solution
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+	   
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.nextLine();
+        int[] a = new int[n];
+        for(int i=0;i<n;++i){
+            a[i]=Integer.parseInt(scan.nextLine());
+        }
+        scan.close();
+
+        // Prints each sequential element in array a
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
