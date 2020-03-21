7b835d9b788e57d85aa9ea73e31161b58e2fb5b7
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-03-21 16:28:58 +0200

    Update Solution.java

--- Introduction/Java Static Initializer Block/Solution.java
+++ Introduction/Java Static Initializer Block/Solution.java
@@ -7,27 +7,27 @@ import java.math.*;
 import java.util.regex.*;
 
 public class Solution {
+    private static int B;
+    private static int H;
+    private static boolean flag;
 
-static boolean flag; 
-static int B,H;
-static{ 
-    Scanner in = new Scanner(System.in);
-    B = in.nextInt();
-    H = in.nextInt(); 
-    if(B>0 && H>0){
-        flag = true;
+    static {
+        Scanner scan = new Scanner(System.in);
+        B = scan.nextInt();
+        H = scan.nextInt();
+        scan.close();
+        if (B <= 0 || H <= 0) {
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+            flag = false;
+        } else {
+            flag = true;
+        }
     }
-    else{
-        System.out.println("java.lang.Exception: Breadth and height must be positive");  
-    }
-}
 
-public static void main(String[] args){
-		if(flag){
-			int area=B*H;
-			System.out.print(area);
-		}
-		
-	}//end of main
-
-}//end of class
+    public static void main(String[] args) {
+        if (flag) {
+            int area = B * H;
+            System.out.print(area);
+        }
+    } // end of main
+} // end of class
