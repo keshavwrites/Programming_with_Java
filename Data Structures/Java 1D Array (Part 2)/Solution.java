7ca90f4c1cd205f5d08bece064a54fb13e0c7722
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-03-21 19:30:29 +0200

    Update Solution.java

--- Data Structures/Java 1D Array (Part 2)/Solution.java
+++ Data Structures/Java 1D Array (Part 2)/Solution.java
@@ -5,29 +5,24 @@ import java.util.*;
 public class Solution {
 
 public static boolean canWin(int leap, int[] game) {
-    if (game == null) {
-        return false;
-    }
+    if (game == null)   return false;
     return isSolvable(leap, game, 0);
-}
+}  
 
 private static boolean isSolvable(int leap, int[] game, int i) {
     // Base Cases
-    if (i >= game.length) {
-        return true;
-    } else if (i < 0 || game[i] == 1) {
-        return false;
-    }
+    if (i >= game.length)            return true;
+    else if (i < 0 || game[i] == 1)  return false;
     
     game[i] = 1; // marks as visited
 
-    // Recursive Cases (Tries +m first to try to finish game quickly)
+    // Recursive Cases (Tries +leap first to try to finish game quickly)
     return isSolvable(leap, game, i + leap)
         || isSolvable(leap, game, i + 1)
         || isSolvable(leap, game, i - 1);
 }
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int q = scan.nextInt();
         while (q-- > 0) {
