3c17e4e12e3f6b5d2ecc786bbe76ef6f1f93b246
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-03-23 01:08:07 +0200

    Update Making Anagrams.java

--- Algorithms/Strings/Making Anagrams.java
+++ Algorithms/Strings/Making Anagrams.java
@@ -13,20 +13,20 @@ public class Solution {
         char c = ' ';
         int cnt = 0;
 
-        while(true){
-            c = (char)System.in.read();
-            if(c=='\n') break;
-            ++chararr[c-'a'];
+        while (true) {
+            c = (char) System.in.read();
+            if (c == '\n') break;
+            ++chararr[c - 'a'];
         }
         in.reset();
-        while(true){
-            c = (char)System.in.read();
-            if(c < 'a' || c > 'z') break;
-            --chararr[c-'a'];
+        while (true) {
+            c = (char) System.in.read();
+            if (c < 'a' || c > 'z') break;
+            --chararr[c - 'a'];
         }
         int i;
-        for (int x : chararr){
-            if(x!=0) cnt+=x>0?x:-x;
+        for (int x : chararr) {
+            if (x != 0) cnt += x > 0 ? x : -x;
         }
         System.out.println(cnt);
     }
