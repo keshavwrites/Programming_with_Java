64a2032503ca84a1f89906a4aabef1afdfddbed4
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-03-23 01:07:33 +0200

    Create Making Anagrams.java

---
+++ Algorithms/Strings/Making Anagrams.java
@@ -0,0 +1,33 @@
+// github.com/andy489
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+
+        int[] chararr = new int[26];
+
+        char c = ' ';
+        int cnt = 0;
+
+        while(true){
+            c = (char)System.in.read();
+            if(c=='\n') break;
+            ++chararr[c-'a'];
+        }
+        in.reset();
+        while(true){
+            c = (char)System.in.read();
+            if(c < 'a' || c > 'z') break;
+            --chararr[c-'a'];
+        }
+        int i;
+        for (int x : chararr){
+            if(x!=0) cnt+=x>0?x:-x;
+        }
+        System.out.println(cnt);
+    }
+}
