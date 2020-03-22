5046eab150b1be541f2cd1f35b320dce913151ca
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-03-22 23:10:43 +0200

    Update Insertion Sort - Part 2.java

--- Algorithms/Sorting/Insertion Sort - Part 2.java
+++ Algorithms/Sorting/Insertion Sort - Part 2.java
@@ -38,3 +38,41 @@ public class Solution {
         insertionSort2(n,arr);
     }
 }
+
+// or without using swap function:
+
+import java.util.Scanner;
+
+public class Solution {
+    
+    private static void print(int[] arr) {
+        for (int x : arr) {
+            System.out.print(x + " " );
+        }
+        System.out.println();
+    }
+
+    private static void insertionSort2(int[] arr) {
+        int i, n = arr.length;
+        for (i = 1; i < n; ++i) {
+            int el = arr[i];
+            int j = i;
+            while (j > 0 && arr[j - 1] > el) {
+                arr[j] = arr[j-1];
+                --j;
+            }
+            arr[j] = el;
+            print(arr);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt(), i;
+        int[] arr = new int[n];
+        for (i = 0; i < n; ++i) {
+            arr[i] = in.nextInt();
+        }
+        insertionSort2(arr);
+    }
+}
