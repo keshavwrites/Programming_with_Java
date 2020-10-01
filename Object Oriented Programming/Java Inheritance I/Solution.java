6d042bddca20f5beb15c20d66b9760914dbabf49
keshavwrites <71269511+keshavwrites@users.noreply.github.com>
2020-10-02 02:45:56 +0530

    Update Solution.java

--- Object Oriented Programming/Java Inheritance I/Solution.java
+++ Object Oriented Programming/Java Inheritance I/Solution.java
@@ -11,8 +11,8 @@ class Animal {
         System.out.println("I am walking");
     }
 }
-
-class Bird extends Animal {
+//since the Bird class is inheriting Animal class. It make Bird class to have characteristics of Animal class i.e. objects of bird can access members of animal.
+class Bird extends Animal {               
     void fly() {
         System.out.println("I am flying");
     }
@@ -24,7 +24,7 @@ class Bird extends Animal {
 public class Solution {
    public static void main(String args[]) {
       Bird bird = new Bird();
-      bird.walk();
+      bird.walk();   //object of Bird calling member function of Animal class.
       bird.fly();
       bird.sing();
    }
